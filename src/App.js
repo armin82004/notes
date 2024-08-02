ef4ec7dc67ae02cd6b430f2da06ecb0e9ef655e1
armin82004 <armin82004@gmail.com>
2024-08-02 16:49:44 +0330

    changing App.js for creating new version of app

--- src/App.js
+++ src/App.js
@@ -1,73 +1,7 @@
-import { useEffect, useState } from "react";
-import { nanoid } from "nanoid";
-import NotesList from "./components/NotesList";
-import Search from "./components/Search";
-import Header from "./components/Header";
+
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "this is my first note",
-      date: "15/02/2024",
-    },
-  ]);
 
-  useEffect(() => {
-    const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
-    if(savedNotes){
-      setNotes(savedNotes);
-    }
-  },[]);
-
-  useEffect(() => {
-    localStorage.setItem('react-notes-app-data', JSON.stringify(notes));
-  }, [notes]);
-
-  const [searchText, setSearchText] = useState("");
-
-
-  const addNote = (text) => {
-    const date = new Date();
-    const newNote = {
-      id: nanoid(),
-      text: text,
-      date: date.toLocaleDateString(),
-    };
-
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
-  };
-
-
-  const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
-
-  const updateNote = (id, newText) => {
-    const newNotes = notes.map((note) =>
-      note.id === id ? { ...note, text: newText } : note
-    );
-    setNotes(newNotes);
-  };
-
-  return (
-    <div>
-      <div className="container">
-        <Header />
-        <Search handleSearchNote={setSearchText} />
-        <NotesList
-					notes={notes.filter((note) =>
-						note.text.toLowerCase().includes(searchText)
-					)}
-          handleAddNote={addNote}
-          handleDeleteNote={deleteNote}
-          handleUpdateNote={updateNote}
-        />
-      </div>
-    </div>
-  );
-};
+}
 
 export default App;
