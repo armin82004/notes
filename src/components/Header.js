6eb4e2f601b455d2f7cbbb886f608318dae143be
armin82004 <armin82004@gmail.com>
2024-08-02 18:17:05 +0330

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import { IoMdMenu } from "react-icons/io";
+import { IoMdSearch } from "react-icons/io";
+import logo from "../images/logo192.png";
 
 const Header = () => {
-  <div className="header-container">
-    <h1>hello</h1>
-    <IoMdMenu />
-  </div>;
+  return (
+    <div className="header-container">
+      <div className="menu-container">
+        <span className="menu">
+          <IoMdMenu className="menu-button" size="1.5em" />
+        </span>
+        <img className="logo" src={logo} alt="logo" />
+        <h1>Notes</h1>
+      </div>
+      <span className="menu search">
+      <IoMdSearch size="1.5em" />
+      </span>
+    </div>
+  );
 };
 
 export default Header;
