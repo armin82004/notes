1bbed0394759c75a441591f20bc6879fb6c81ac8
armin82004 <armin82004@gmail.com>
2024-08-06 21:31:30 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useId } from "react";
 import AddNote from "./components/AddNote";
 import Header from "./components/Header";
 
 const App = () => {
+  const default_text = [{
+    id:useId,
+    text:"hello",
+    title:"title",
+    date:"27/01/2024"
+  }]
   return(
     <div>
       <Header/>
