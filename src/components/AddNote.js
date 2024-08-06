a7badd12f823e08ef4c23758945e952e3d7dc358
armin82004 <armin82004@gmail.com>
2024-08-06 14:10:39 +0330

    AddNote component

---
+++ src/components/AddNote.js
@@ -0,0 +1,9 @@
+const AddNote = ()=>{
+    return(
+        <div className="add-note-container">
+            <input className="add-note-input"/>
+        </div>
+    );
+}
+
+export default AddNote;
