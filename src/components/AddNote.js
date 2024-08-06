dce0cdd43ec5dc00ba35e72689b7b25c93777f85
armin82004 <armin82004@gmail.com>
2024-08-06 14:22:13 +0330

    .

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,9 +1,22 @@
-const AddNote = ()=>{
-    return(
-        <div className="add-note-container">
-            <input className="add-note-input"/>
-        </div>
+import { useState } from "react";
+
+const AddNote = () => {
+  const [isAdding,setisAdding] = useState(false);
+  function handleinputclick(){
+    setisAdding(true);
+  }
+  {
+    isAdding ? (
+      <div className="add-note-container">
+        <input className="input-note-title" onClick={()=>{handleinputclick()}}/>
+      </div>
+    ) : (
+      <div className="add-note-container">
+        <input className="input-note-title" />
+        <input className="input-note-content" />
+      </div>
     );
-}
+  }
+};
 
 export default AddNote;
