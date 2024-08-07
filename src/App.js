0170934b489bf882e29de75dec883d41704bbb61
armin82004 <armin82004@gmail.com>
2024-08-07 13:10:20 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import { useId } from "react";
+import { useId, useState } from "react";
 import AddNote from "./components/AddNote";
 import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 
 const App = () => {
-  const default_text = [{
-    id:useId,
-    text:"hello",
-    title:"title",
-    date:"27/01/2024",
-    color:"red"
-  }]
-  function handleaddnote(text,color,title){
+  const [notes, setnotes] = useState([
+    {
+      id: useId,
+      text: "hello",
+      title: "title",
+      date: "12:34 pm",
+      color: "default",
+    },
+  ]);
+  function handleaddnote(text, color, title) {
+    const hours = Date.getHours();
+    const min = Date.getMinutes();
+    const date = hours + ":" + min;
+    const newnote = {
+      id: useId,
+      text: text,
+      color: color,
+      title: title,
+      date: date,
+    };
 
+    const newnotes = [...notes,newnote];
+    setnotes(newnotes);
   }
-  return(
+  return (
     <div>
-      <Header/>
-      <AddNote handleaddnote={handleaddnote}/>
-      <NotesList/>
+      <Header />
+      <AddNote handleaddnote={handleaddnote} />
+      <NotesList />
     </div>
   );
-}
+};
 export default App;
